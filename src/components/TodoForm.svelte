5cfebecff8f101bf360019eb50b77a9e657c55ac
PranjalAgni <pranjal.agnihotri6@gmail.com>
2024-03-14 11:54:59 +0530

    :rabbit: a dumb todo app with svelte

---
+++ src/components/TodoForm.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let addTodo: (todo: string) => void;
+	let newTodo = "";
+	function handleSubmit() {
+		addTodo(newTodo);
+		newTodo = "";
+	}
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+	<label>
+		New Todo
+		<input bind:value={newTodo} placeholder="Enter a todo " />
+	</label>
+	<button class="container mw-100">Add todo</button>
+</form>
+
+<style>
+	.mw-100 {
+		max-width: 100%;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import TodoForm from "../components/TodoForm.svelte";
+	let todos: { title: string; done: boolean }[] = [];
+	function addTodo(todo: string) {
+		todos = [
+			...todos,
+			{
+				title: todo,
+				done: false
+			}
+		];
+	}
+
+	function removeTodo(id: number) {
+		todos.splice(id, 1);
+		todos = todos;
+	}
+
+	$: console.log("todos", todos);
+</script>
+
+<main>
+	<TodoForm {addTodo} />
+	<ul>
+		{#each todos as todo, index}
+			<div>
+				<button class="unset" on:click={() => (todo.done = !todo.done)}>
+					<li class:done={todo.done}>
+						{#if todo.done}
+							<span>✅</span>
+						{/if}
+						<span>{todo.title}</span>
+						<button class="unset" on:click={() => removeTodo(index)}>
+							<span class="delete">🗑️</span>
+						</button>
+					</li>
+				</button>
+			</div>
+		{/each}
+	</ul>
+</main>
+
+<style>
+	.done {
+		text-decoration: line-through;
+	}
+
+	.unset {
+		all: unset;
+	}
+
+	.delete {
+		display: inline-block;
+	}
+
+	.delete:hover {
+		transform: scale(2);
+	}
+</style>
